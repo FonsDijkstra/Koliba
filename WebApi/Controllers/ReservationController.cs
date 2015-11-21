ba2e8b1ff6ca2cdb480fc8d7307a40c486b6cf35
FonsDijkstra <fons.dijkstra@gmail.com>
2015-11-21 17:42:47 +0100

    OpeningTimes api defined

--- WebApi/Controllers/ReservationController.cs
+++ WebApi/Controllers/ReservationController.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Koliba.WebApi.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
-namespace WebApi.Controllers
+namespace Koliba.WebApi.Controllers
 {
     public class ReservationController : ApiController
     {
+        public IEnumerable<OpeningTime> GetOpeningTimes()
+        {
+            return new[]
+            {
+                new OpeningTime { Start = DateTime.Today.AddHours(14), Duration = new TimeSpan(8, 0, 0) },
+                new OpeningTime { Start = DateTime.Today.AddDays(1).AddHours(12), Duration = new TimeSpan(10, 0, 0) },
+            };
+        }
     }
 }
