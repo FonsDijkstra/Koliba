3b54a28631c673fa6c742d86c645d78e67d07424
FonsDijkstra <fons.dijkstra@gmail.com>
2015-11-22 09:55:16 +0100

    Add default customer view

---
+++ Apps.Tests/Controllers/CustomerControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Koliba.Apps.Controllers;
+using Xunit;
+
+namespace Koliba.Apps.Tests.Controllers
+{
+    public class CustomerControllerTest
+    {
+        readonly CustomerController sut;
+
+        public CustomerControllerTest()
+        {
+            sut = new CustomerController();
+        }
+
+        [Fact]
+        public void Index_returns_view()
+        {
+            Assert.NotNull(sut.Index() is ViewResult);
+        }
+    }
+}
