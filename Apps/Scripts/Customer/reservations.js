43b4b6907407a8e376bb8e5021a37a0b8be82ffa
FonsDijkstra <fons.dijkstra@gmail.com>
2015-11-24 21:05:03 +0100

    Eerste stukje react

---
+++ Apps/Scripts/Customer/reservations.js
@@ -0,0 +1,16 @@
+﻿var ReservationsBox = React.createClass({
+    render: function() {
+        return (
+            React.createElement('a', { 'href': '#', styleName: 'display:block'},
+                React.createElement('div', { className: 'panel panel-default' },
+                    React.createElement('div', { className: 'panel-body' },
+                        React.createElement('h2', null,
+                            "ToDo"))))
+        );
+    }
+});
+
+ReactDOM.render(
+    React.createElement(ReservationsBox, null),
+    document.getElementById('reservations-content')
+);
