dc4e319e1c1ad7407273509f2144df69ea94e3f2
FonsDijkstra <fons.dijkstra@gmail.com>
2015-11-26 20:25:31 +0100

    Started with date selector

--- Apps/Scripts/Customer/reservations.js
+++ Apps/Scripts/Customer/reservations.js
@@ -1,13 +1,42 @@
 ﻿$.get('api/reservation/resources', function (resources, status) {
 
     var ReservationsBox = React.createClass({
+        onClick: function () {
+            ReactDOM.render(
+                React.createElement(DateSelector, null),
+                document.getElementById('reservations-content')
+            );
+        },
         render: function () {
             return (
-                React.createElement('a', { 'href': '#', styleName: 'display:block' },
+                React.createElement('a', { 'href': '#', styleName: 'display:block', 'onClick': this.onClick },
                     React.createElement('div', { className: 'panel panel-default' },
                         React.createElement('div', { className: 'panel-body' },
                             React.createElement('h2', null,
                                 resources.ReservationsHeader))))
+            );
+        }
+    });
+
+    var SpecificDate = React.createClass({
+        render: function () {
+            return (
+                React.createElement('button', null, this.props.date)
+            );
+        }
+    });
+
+    var DateSelector = React.createClass({
+        getInitialState: function () {
+            return { dates: ['test1', 'test2'] };
+        },
+        render: function () {
+            var specificDates = this.state.dates.map(function (date) {
+                return React.createElement(SpecificDate, { 'date': date }, null);
+            });
+            return (
+                React.createElement('div', null,
+                    specificDates)
             );
         }
     });
