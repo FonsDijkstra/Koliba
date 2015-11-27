2aa096b871baea3ecfdb40153364340d2515f796
FonsDijkstra <fons.dijkstra@gmail.com>
2015-11-27 21:34:40 +0100

    NextDay als extension method

--- Business/OpeningTimes.cs
+++ Business/OpeningTimes.cs
@@ -72,34 +72,13 @@ namespace Koliba.Business
             }
 
             for (int i = 1; i <= nofDaysAhead; ++i) {
-                day = NextDay(day);
+                day = day.NextDay();
                 if (Schedule.TryGetValue(day, out openingTime)) {
                     yield return new OpeningDate {
                         Start = from.Date.AddDays(i).Add(openingTime.Open),
                         Duration = openingTime.Duration,
                     };
                 }
-            }
-        }
-
-        static DayOfWeek NextDay(DayOfWeek day)
-        {
-            if (day == DayOfWeek.Monday) {
-                return DayOfWeek.Tuesday;
-            } else if (day == DayOfWeek.Tuesday) {
-                return DayOfWeek.Wednesday;
-            } else if (day == DayOfWeek.Wednesday) {
-                return DayOfWeek.Thursday;
-            } else if (day == DayOfWeek.Thursday) {
-                return DayOfWeek.Friday;
-            } else if (day == DayOfWeek.Friday) {
-                return DayOfWeek.Saturday;
-            } else if (day == DayOfWeek.Saturday) {
-                return DayOfWeek.Sunday;
-            } else if (day == DayOfWeek.Sunday) {
-                return DayOfWeek.Monday;
-            } else {
-                throw new ArgumentOutOfRangeException(nameof(day));
             }
         }
     }
---
+++ Business/Tools.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koliba.Business
+{
+    public static class Tools
+    {
+        public static DayOfWeek NextDay(this DayOfWeek day)
+        {
+            if (day == DayOfWeek.Monday) {
+                return DayOfWeek.Tuesday;
+            } else if (day == DayOfWeek.Tuesday) {
+                return DayOfWeek.Wednesday;
+            } else if (day == DayOfWeek.Wednesday) {
+                return DayOfWeek.Thursday;
+            } else if (day == DayOfWeek.Thursday) {
+                return DayOfWeek.Friday;
+            } else if (day == DayOfWeek.Friday) {
+                return DayOfWeek.Saturday;
+            } else if (day == DayOfWeek.Saturday) {
+                return DayOfWeek.Sunday;
+            } else if (day == DayOfWeek.Sunday) {
+                return DayOfWeek.Monday;
+            } else {
+                throw new ArgumentOutOfRangeException(nameof(day));
+            }
+        }
+    }
+}
